30174f365fbab524d77b404868ff1bcce9d1e92d
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-11-12 23:54:14 -0300

    refactor: arrange routes namings

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,7 @@
 export const ROUTES = {
 	AUTH: {
-		SIGN_IN: () => '/sign-in',
-		SIGN_UP: () => '/sign-up',
+		SIGN_IN: () => '/auth/sign-in',
+		SIGN_UP: () => '/auth/sign-up',
 	},
 	DASHBOARD: {
 		HOME: () => '/dashboard',
