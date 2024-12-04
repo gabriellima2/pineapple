2b27da00b063274c8653d160db909d33ab095406
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-12-03 22:46:10 -0300

    fix: show feedback toast on delete service or customer

--- src/app/dashboard/customers/_components/delete-customer/hooks/use-delete-customer.ts
+++ src/app/dashboard/customers/_components/delete-customer/hooks/use-delete-customer.ts
@@ -1,6 +1,9 @@
 import { useState } from 'react'
+import { useTranslations } from 'next-intl'
 
 import { useCustomersContext } from '../../../_contexts/customers.context'
+import { useToast } from '@/hooks/use-toast'
+
 import { deleteCustomer } from '../../../_actions/customer-action'
 
 type UseDeleteCustomerParams = {
@@ -9,16 +12,23 @@ type UseDeleteCustomerParams = {
 
 export function useDeleteCustomer(params: UseDeleteCustomerParams) {
 	const { customerId } = params
+	const t = useTranslations('dashboard.customers.delete.notification')
 	const [isDeleting, setIsDeleting] = useState(false)
 	const { closeDeleteCustomer } = useCustomersContext()
+	const { toast } = useToast()
 
 	async function handleDelete() {
 		setIsDeleting(true)
 		try {
 			await deleteCustomer(customerId)
 			closeDeleteCustomer()
+			toast({
+				title: t('success.title'),
+				description: t('success.description'),
+			})
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		} catch (_) {
-			console.log(_)
+			toast({ title: t('error.title'), description: t('error.description') })
 		} finally {
 			setIsDeleting(false)
 		}
--- src/app/dashboard/services/_components/delete-service/hooks/use-delete-service.ts
+++ src/app/dashboard/services/_components/delete-service/hooks/use-delete-service.ts
@@ -1,6 +1,9 @@
 import { useState } from 'react'
+import { useTranslations } from 'next-intl'
 
 import { useServicesContext } from '../../../_contexts/services.context'
+import { useToast } from '@/hooks/use-toast'
+
 import { deleteService } from '../../../_actions/service-action'
 
 type UseDeleteServiceParams = {
@@ -9,16 +12,23 @@ type UseDeleteServiceParams = {
 
 export function useDeleteService(params: UseDeleteServiceParams) {
 	const { serviceId } = params
+	const t = useTranslations('dashboard.services.delete.notification')
 	const [isDeleting, setIsDeleting] = useState(false)
 	const { closeDeleteService } = useServicesContext()
+	const { toast } = useToast()
 
 	async function handleDelete() {
 		setIsDeleting(true)
 		try {
 			await deleteService(serviceId)
 			closeDeleteService()
+			toast({
+				title: t('success.title'),
+				description: t('success.description'),
+			})
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		} catch (_) {
-			console.log(_)
+			toast({ title: t('error.title'), description: t('error.description') })
 		} finally {
 			setIsDeleting(false)
 		}
