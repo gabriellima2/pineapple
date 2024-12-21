e3af6442ce287361b8b7ec2ccf7f5e3a04798ece
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-12-21 15:55:06 -0300

    fix: add field-array validation on submit

--- src/app/dashboard/service-history/_components/create-service-history/hooks/use-create-service-history-form.ts
+++ src/app/dashboard/service-history/_components/create-service-history/hooks/use-create-service-history-form.ts
@@ -1,8 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useTranslations } from 'next-intl'
 import { useForm } from 'react-hook-form'
 
 import { useCreateServiceHistoryMutation } from '../../../_hooks/mutations/use-create-service-history-mutation'
 import { useServiceHistoryContext } from '../../../_contexts/service-history.context'
+import { useToast } from '@/hooks/use-toast'
 
 import {
 	useGetCreateServiceHistoryIntlSchema,
@@ -10,6 +12,8 @@ import {
 } from '../../../_hooks/schemas/use-get-create-service-history-intl-schema'
 
 export function useCreateServiceHistoryForm() {
+	const t = useTranslations()
+	const { toast } = useToast()
 	const { closeCreateServiceHistory } = useServiceHistoryContext()
 	const { intlSchema } = useGetCreateServiceHistoryIntlSchema()
 	const { isCreating, createServiceHistory } = useCreateServiceHistoryMutation({
@@ -31,6 +35,15 @@ export function useCreateServiceHistoryForm() {
 	})
 
 	async function onSubmit(data: CreateServiceHistoryFields) {
+		if (!data.services.length) {
+			toast({
+				title: t(
+					'dashboard.service-history.create.validations.min_services_length'
+				),
+				variant: 'destructive',
+			})
+			return
+		}
 		await createServiceHistory(data)
 	}
 
